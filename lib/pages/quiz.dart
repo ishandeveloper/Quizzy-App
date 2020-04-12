f186fbd40f0b687c5de4820bee4221ae73cbeae7
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-12 14:13:56 +0530

    UI

--- lib/pages/quiz.dart
+++ lib/pages/quiz.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 
 import 'about.dart';
 
-class QuizHome extends StatelessWidget {
+class QuizHome extends StatefulWidget {
+  @override
+  _QuizHomeState createState() => _QuizHomeState();
+}
 
-
+class _QuizHomeState extends State<QuizHome> {
   var progress;
 
   @override
   Widget build(BuildContext context) {
-
-    progress=0.75;
+    progress = 0.75;
 
     return Scaffold(
       backgroundColor: Colors.deepPurple[900],
@@ -18,9 +20,18 @@ class QuizHome extends StatelessWidget {
         backgroundColor: Colors.deepPurple[900],
         elevation: 0,
         leading: Icon(Icons.question_answer),
-        title: Text(
-          'Quizzy',
-          style: TextStyle(fontFamily: 'Poppins'),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Quizzy',
+              style: TextStyle(fontFamily: 'Poppins'),
+            ),
+            Text(
+              "Made with ♥ by ishandeveloper",
+              style: TextStyle(fontFamily: 'Poppins', fontSize: 10),
+            ),
+          ],
         ),
         actions: <Widget>[
           GestureDetector(
@@ -28,8 +39,9 @@ class QuizHome extends StatelessWidget {
               child: Icon(Icons.info),
               margin: EdgeInsets.only(right: 20),
             ),
-            onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>AboutPage()));
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => AboutPage()));
             },
           )
         ],
@@ -40,6 +52,109 @@ class QuizHome extends StatelessWidget {
             backgroundColor: Colors.deepPurple[900],
             valueColor: new AlwaysStoppedAnimation<Color>(Colors.white),
             value: progress,
+          ),
+          Card(
+            margin:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
+            color: Colors.deepPurple[900],
+            elevation: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.deepPurple[700],
+                  borderRadius: BorderRadius.circular(20)),
+              padding: EdgeInsets.all(30),
+              height: MediaQuery.of(context).size.height * 0.5,
+              width: MediaQuery.of(context).size.width * 0.75,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Question',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Poppins',
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  Text(
+                    'This is a dummy question. Are you feeling good?',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Poppins',
+                      fontSize: 26,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: <Widget>[
+                MaterialButton(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(15),
+                  minWidth: MediaQuery.of(context).size.width * 0.75,
+                  child: Text(
+                    'True',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  onPressed: () {},
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.01,
+                ),
+                MaterialButton(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(15),
+                  minWidth: MediaQuery.of(context).size.width * 0.75,
+                  child: Text(
+                    'False',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: <Widget>[
+                // Container(
+                //   color: Colors.black,
+                //   width: 100,
+                //   height: 50,
+                // ),
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.check,
+                        color: Colors.green[300],
+                      ),
+                      SizedBox(width: 10,),
+                      Icon(
+                        Icons.close,
+                        color: Colors.red[400],
+                      ),
+                    ],
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 30),
+                )
+              ],
+            ),
           )
         ],
       ),
