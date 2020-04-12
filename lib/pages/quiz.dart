90f04d531243e194cbfb2a9736d63583c68eb671
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-12 15:50:42 +0530

    Question Bank

--- lib/pages/quiz.dart
+++ lib/pages/quiz.dart
@@ -1,6 +1,8 @@
+import 'package:Quiz/model/questions.dart';
 import 'package:flutter/material.dart';
 
 import 'about.dart';
+import 'package:Quiz/model/questions.dart';
 
 class QuizHome extends StatefulWidget {
   @override
@@ -9,33 +11,60 @@ class QuizHome extends StatefulWidget {
 
 class _QuizHomeState extends State<QuizHome> {
   List<Widget> scorekeeper = [];
-  List<String> questions = [
-    'This is a dummy question. Are you feeling good?',
-    'This is another dummy question. Are you feeling good?',
-    'This is third dummy question. Are you feeling good?',
-    'This is fourth dummy question. Are you feeling good?',
-    'This is 6 dummy question. Are you feeling good?',
-    'This is 7 dummy question. Are you feeling good?',
-    'This is 8 dummy question. Are you feeling good?',
-    'This is 9 dummy question. Are you feeling good?',
-    'This is last dummy question. Are you feeling good?',
-    'This is 6 dummy question. Are you feeling good?',
-    'This is 7 dummy question. Are you feeling good?',
-    'This is 8 dummy question. Are you feeling good?',
+  List<Question> questionBank = [
+    Question(
+        q: 'Only character or integer can be used in switch statement in C Programming',
+        a: false),
+    Question(
+        q: 'The return type of malloc function is void in C Programming',
+        a: false),
+    Question(
+        q: '#define is known as preprocessor compiler directive in C Programming',
+        a: true),
+    Question(
+        q: 'sizeof( ) is a function that returns the size of a variable in C Programming',
+        a: false),
+    Question(
+        q: 'The ++ operator increments the operand by 1, whereas, the -- operator decrements it by 1, in C Programming',
+        a: true),
+    Question(
+        q: 'Only character or integer can be used in switch statement in C Programming',
+        a: false),
+    Question(
+        q: 'It is necessary that a loop counter must only be an int. It cannot be a float in C Programming',
+        a: false),
+    Question(
+        q: 'A zero value is considered to be false and a non-zero value is considered to be true in C Programming',
+        a: true),
   ];
-  List<bool> answers = [
-    true,
-    false,
-    true,
-    true,
-    false,
-    true,
-    true,
-    false,
-    false,
-    false,
-    false
-  ];
+  // Question q1= Question(q:'This is a dummy question. Are you feeling good?',a:true);
+  // List<String> questions = [
+  //   'This is a dummy question. Are you feeling good?',
+  //   'This is another dummy question. Are you feeling good?',
+  //   'This is third dummy question. Are you feeling good?',
+  //   'This is fourth dummy question. Are you feeling good?',
+  //   'This is 6 dummy question. Are you feeling good?',
+  //   'This is 7 dummy question. Are you feeling good?',
+  //   'This is 8 dummy question. Are you feeling good?',
+  //   'This is 9 dummy question. Are you feeling good?',
+  //   'This is last dummy question. Are you feeling good?',
+  //   'This is 6 dummy question. Are you feeling good?',
+  //   'This is 7 dummy question. Are you feeling good?',
+  //   'This is 8 dummy question. Are you feeling good?',
+  // ];
+  // List<bool> answers = [
+  //   true,
+  //   false,
+  //   true,
+  //   true,
+  //   false,
+  //   true,
+  //   true,
+  //   false,
+  //   false,
+  //   false,
+  //   false
+  // ];
   var progress;
   var selectedquestion = 'This is a dummy question. Are you feeling good?';
   int i = 0;
