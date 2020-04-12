28be1a85b96f128d9fe6893c573d075695b09463
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-12 15:58:43 +0530

    Optimized

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'pages/quiz.dart';
 
-void main()=>(runApp(QuizApp()));
+void main() => (runApp(QuizApp()));
 
 class QuizApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:QuizHome()
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: QuizHome());
   }
 }
