ee855dd46d4df466846ffdc666c8d4258cfa7b64
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-12 15:58:35 +0530

    Optimized

--- lib/resources/questionBank.dart
+++ lib/resources/questionBank.dart
@@ -1,37 +1,37 @@
-  import 'package:Quiz/model/questions.dart';
+import 'package:Quiz/model/questions.dart';
 
 List<Question> questionBank = [
-    Question(
-        q: 'Only character or integer can be used in switch statement in C Programming',
-        a: false),
-    Question(
-        q: 'The return type of malloc function is void in C Programming',
-        a: false),
-    Question(
-        q: '#define is known as preprocessor compiler directive in C Programming',
-        a: true),
-    Question(
-        q: 'sizeof( ) is a function that returns the size of a variable in C Programming',
-        a: false),
-    Question(
-        q: 'The ++ operator increments the operand by 1, whereas, the -- operator decrements it by 1, in C Programming',
-        a: true),
-    Question(
-        q: 'Only character or integer can be used in switch statement in C Programming',
-        a: false),
-    Question(
-        q: 'It is necessary that a loop counter must only be an int. It cannot be a float in C Programming',
-        a: false),
-    Question(
-        q: 'A zero value is considered to be false and a non-zero value is considered to be true in C Programming',
-        a: true),
-    Question(
-        q: '= is used for comparison, whereas, == is used for assignment of two quantities in C Programming',
-        a: false),
-    Question(
-        q: 'Blank spaces may be inserted between two words to improve the readability of the statement in C Programming',
-        a: true),
-    Question(
-        q: 'The keywords cannot be used as variable names in C Programming',
-        a: true),
-  ];
+  Question(
+      q: 'Only character or integer can be used in switch statement in C Programming',
+      a: false),
+  Question(
+      q: 'The return type of malloc function is void in C Programming',
+      a: false),
+  Question(
+      q: '#define is known as preprocessor compiler directive in C Programming',
+      a: true),
+  Question(
+      q: 'sizeof( ) is a function that returns the size of a variable in C Programming',
+      a: false),
+  Question(
+      q: 'The ++ operator increments the operand by 1, whereas, the -- operator decrements it by 1, in C Programming',
+      a: true),
+  Question(
+      q: 'Only character or integer can be used in switch statement in C Programming',
+      a: false),
+  Question(
+      q: 'It is necessary that a loop counter must only be an int. It cannot be a float in C Programming',
+      a: false),
+  Question(
+      q: 'A zero value is considered to be false and a non-zero value is considered to be true in C Programming',
+      a: true),
+  Question(
+      q: '= is used for comparison, whereas, == is used for assignment of two quantities in C Programming',
+      a: false),
+  Question(
+      q: 'Blank spaces may be inserted between two words to improve the readability of the statement in C Programming',
+      a: true),
+  Question(
+      q: 'The keywords cannot be used as variable names in C Programming',
+      a: true),
+];
