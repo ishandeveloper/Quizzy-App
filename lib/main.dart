da80b9e896baa3c15db07b1c368c32025a0739b6
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-12 13:05:51 +0530

    Initialize Material App

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main()=>(runApp(QuizHome()));
+
+class QuizHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
